b45ad30ef85ec29749a962446f89c9cefdfc7ff1
2dam <2dam@MACINF02.TartangaLH.eus>
2020-10-19 14:07:41 +0200

    First commit of the libraries, classes created

---
+++ src/user_message/User.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user_message;
+
+/**
+ *
+ * @author 2dam
+ */
+public class User {
+    public int hola() {
+        return 0;
+    }
+}
