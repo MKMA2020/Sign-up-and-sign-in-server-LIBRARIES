97a13254a06b9a7807b930b6a3a9ece12b2bafb2
2dam <2dam@MACINF04.TartangaLH.eus>
2020-10-28 12:05:03 +0100

    DataBaseConnectionException added

---
+++ src/exceptions/DataBaseConnectionException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Exception triggered by failing to connect to the database.
+ * @author Aitor Garcia
+ */
+public class DataBaseConnectionException {
+    public DataBaseConnectionException(){
+        final Logger LOG = Logger.getLogger("exceptions.DataBaseConnectionException.java");
+        LOG.log(Level.SEVERE, "There was an error attempting to connect to the database");
+    }
+}
