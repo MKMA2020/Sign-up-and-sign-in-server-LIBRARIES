16487e4db636ceeb4829c40bbd5200136181a5ca
Martin Gros <>
2020-11-02 19:26:02 +0100

    Message Class commented and ready.

--- src/user_message/Message.java
+++ src/user_message/Message.java
@@ -1,55 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package user_message;
 
 import java.io.Serializable;
 
 /**
- *
- * This class'll be used as the messsage object. Which will be sent across both
- * apps in order to fullfill multiple tasks and report exceptions.
- *
+ * Message class will contain the message object that is
+ * sent between the Server and Client side of the app.
+ * 
  * @author Martin Gros
  */
 public class Message implements Serializable {
-
+    
+    private User user;
+    private MessageType messageType;
+    /**
+     * Class Constructor to create a Message.
+     * @param user from the User class.
+     * @param messageType from the enum MessageType.
+     */
     public Message(User user, MessageType messageType) {
         this.user = user;
         this.messageType = messageType;
     }
     
-    
-
-   
-
-    private User user;   
-    private MessageType messageType;
-
-    public Message() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    /**
+     * @return Returns the user of the message. 
+     */
     public User getUser() {
         return user;
     }
-
+    
+    /**
+     * @param user Sets the user on the message.
+     */
     public void setUser(User user) {
         this.user = user;
     }
-
-   
-
+    
+    /**
+     * @return Returns the messageType of the message.
+     */
     public MessageType getMessageType() {
         return messageType;
     }
-
+    
+    /**
+     * @param messageType Sets the messageType on the message.
+     */
     public void setMessageType(MessageType messageType) {
         this.messageType = messageType;
     }
-
-    
 
 }
