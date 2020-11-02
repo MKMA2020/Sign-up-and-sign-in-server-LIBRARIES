e3eae0bd652511875ad8942a4a42e7f798675575
Martin Valiente Ainz <>
2020-11-02 19:47:40 +0100

    User Class commented and ready.

--- src/user_message/Message.java
+++ src/user_message/Message.java
@@ -3,17 +3,19 @@ package user_message;
 import java.io.Serializable;
 
 /**
- * Message class will contain the message object that is
- * sent between the Server and Client side of the app.
- * 
+ * Message class will contain the message object that is sent between the Server
+ * and Client side of the app.
+ *
  * @author Martin Gros
  */
 public class Message implements Serializable {
-    
+
     private User user;
     private MessageType messageType;
+
     /**
      * Class Constructor to create a Message.
+     *
      * @param user from the User class.
      * @param messageType from the enum MessageType.
      */
@@ -21,28 +23,28 @@ public class Message implements Serializable {
         this.user = user;
         this.messageType = messageType;
     }
-    
+
     /**
-     * @return Returns the user of the message. 
+     * @return Returns the user of the message.
      */
     public User getUser() {
         return user;
     }
-    
+
     /**
      * @param user Sets the user on the message.
      */
     public void setUser(User user) {
         this.user = user;
     }
-    
+
     /**
      * @return Returns the messageType of the message.
      */
     public MessageType getMessageType() {
         return messageType;
     }
-    
+
     /**
      * @param messageType Sets the messageType on the message.
      */
--- src/user_message/User.java
+++ src/user_message/User.java
@@ -1,127 +1,146 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package user_message;
 
 import java.io.Serializable;
 import java.security.Timestamp;
 
-
-
 /**
- * This class contains the user object.
- * It will be sent inside a message to the server
- * and will be either added or checked with the db
+ * User class will be used as a part of the Message that will be sent between
+ * Server and Client.
+ *
  * @author Martin Gros
  */
-public class User implements Serializable{
-
+public class User implements Serializable {
 
     private long id;
     private String login;
     private String email;
     private String fullName;
     private boolean status;
-    private enum privilege{
-        USER,
-        ADMIN
-    }
     private String password;
     private Timestamp lastAccess;
     private Timestamp lastPasswordChange;
-    
+
+    private enum privilege {
+        USER,
+        ADMIN
+    }
+
     /**
-     * 
-     * @return This method will return the User ID long.
+     * @return Returns the user id.
      */
     public long getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id Sets the user id.
+     */
     public void setId(long id) {
         this.id = id;
     }
+
     /**
-     * 
-     * @return This method will return the Username login String.
+     * @return Returns the user login.
      */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * @param login Sets the user login.
+     */
     public void setLogin(String login) {
         this.login = login;
     }
+
     /**
-     * 
-     * @return This method will return the User Email String.
+     * @return Returns the user email.
      */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * @param email Sets the user email.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
+
     /**
-     * 
-     * @return This method will return the full name of the user as a String.
+     * @return Returns the user fullName.
      */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * @param fullName Sets the user fullName.
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
     /**
-     * 
-     * @return This method will return wheter the User status is true or false (True will mean the user is active and False will mean user is inactive).
+     * Boolean user status will be true when the user is active and false when
+     * is inactive.
+     *
+     * @return Returns the user status.
      */
     public boolean isStatus() {
         return status;
     }
 
+    /**
+     * Boolean user status will be true when the user is active and false when
+     * is inactive.
+     *
+     * @param status Sets the user status.
+     */
     public void setStatus(boolean status) {
         this.status = status;
     }
+
     /**
-     * 
-     * @return This method will return the User password as a String.
+     * @return Returns the user password.
      */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * @param password Sets the user password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
+
     /**
-     * 
-     * @return This method will return the Last Access of the User as a Timestamp.
+     * @return Returns the user lastAccess.
      */
     public Timestamp getLastAccess() {
         return lastAccess;
     }
 
+    /**
+     * @param lastAccess Sets the user lastAccess.
+     */
     public void setLastAccess(Timestamp lastAccess) {
         this.lastAccess = lastAccess;
     }
+
     /**
-     * 
-     * @return This method will return the last Password Change of the User as a Timestamp.
+     * @return Returns the user lastPasswordChange.
      */
     public Timestamp getLastPasswordChange() {
         return lastPasswordChange;
     }
 
+    /**
+     * @param lastPasswordChange Sets the user lastPasswordChange.
+     */
     public void setLastPasswordChange(Timestamp lastPasswordChange) {
         this.lastPasswordChange = lastPasswordChange;
     }
 }
-
-
-
-
